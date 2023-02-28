7f3574817bae62056b4c83fc21c0d326d9f28040
Yusuf olatunji  adeshina <72082053+YeezyWest@users.noreply.github.com>
2023-02-28 17:36:39 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [monsters, setMonsters] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => setMonsters(users))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const filteredMonsters = monsters.filter((monster) =>
+    monster.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App py-28">
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl font-bold capitalize text-center pb-5">
+          {' '}
+          Monster Rolodex
+        </h1>
+        <input
+          className="py-4 px-10"
+          type="search"
+          placeholder="Search Monsters"
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        {filteredMonsters.map((monster) => {
+          return (
+            <div className='block' key={monster.id}>
+            <div className=''>
+            <img alt={`monster ${monster.name}`} src={`https://robohash.org/${monster.id}?set=set2&size180x180`} />
+              <h2 className='text-center'>{monster.name}</h2>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
